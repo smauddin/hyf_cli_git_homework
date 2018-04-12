e8d52b78f4db48490a91ab7e0cef762c96e7812e
ShazaMaher <shaza.maher.145@gmail.com>
2018-04-12 13:50:29 +0200

    add a shell script shaza_maher.sh

---
+++ shaza_maher.sh
@@ -0,0 +1,22 @@
+set -x
+mkdir cabbage
+cd cabbage
+pwd
+touch orange.txt
+echo "Hello" > orange.txt
+echo "Hello" > orange.txt
+echo "Hello" > orange.txt
+echo "Hello" > orange.txt
+echo "Hello" > orange.txt
+cp orange.txt 1.txt 
+cp orange.txt 2.txt
+cp orange.txt 3.txt
+cp orange.txt 4.txt
+cp orange.txt 5.txt
+touch pets.txt
+echo "cat" > pets.txt
+echo "dog" >> pets.txt
+echo "hamster" >> pets.txt
+cat pets.txt
+pwd
+
